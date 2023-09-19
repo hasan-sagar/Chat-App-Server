cca15f2a61a2c556d53a960d2313be13085aea0e
hasan-sagar <khsagar0512@gmail.com>
2023-09-19 15:18:04 +0600

    implement socket

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { errorHandler, notFound } from "./middlewares/errorMiddleware";
 import cors from "cors";
 import ChatRouter from "./routes/chat";
 import MessageRouter from "./routes/message";
+import { Server } from "socket.io";
 
 app.use(cors());
 
@@ -25,6 +26,45 @@ app.get("/", (req: Request, res: Response) => {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => {
+const serverConnection = app.listen(process.env.PORT || 5000, () => {
   console.log(`hi ${process.env.PORT}`);
+});
+
+const io = new Server(serverConnection, {
+  pingTimeout: 60000,
+  cors: {
+    origin: ["http://localhost:5000", "http://localhost:5173"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected with socket io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room " + room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user: any) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
 });
